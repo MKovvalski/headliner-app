701ef6c400d971c6434a6cabf1c60492dea0a060
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2020-02-09 18:53:57 +0100

    feat(app): finish styling for aplha build

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Header from './Header'
 import Layout from './Layout'
 import HeadlinersContainer from './HeadlinersContainer'
 import SearchContainer from './SearchContainer'
@@ -7,6 +8,7 @@ import Footer from './footer'
 const App: React.FC = () => {
   return (
     <div className='app'>
+      <Header /> 
       <Layout>
         <SearchContainer />
         <HeadlinersContainer />
---
+++ src/components/Logo.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Logo: React.FC = () => {
+    return (
+        <div className='logo'>
+            <div className='logo__row logo__row--top' />
+            <div className='logo__row logo__row--middle'>
+                <div className='logo__dot' />
+            </div>
+            <div className='logo__row logo__row--bottom' />
+        </div>
+    )
+}
+
+export default Logo
