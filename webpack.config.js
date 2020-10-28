13b484f5f8b28011aa529e08793e8649b1ea7e38
Mateusz Kowalski <matt.kowalski.public@gmail.com>
2020-10-28 10:10:48 +0100

    chore(app): fix lack of TS errors in source map and add core

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const chalkAnimation = require("chalk-animation");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
@@ -21,72 +22,80 @@ const paths = {
     appSrc: resolveApp("src"),
 };
 
-module.exports = {
-    entry: {
-        main: paths.appIndexJs,
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-    },
-    output: {
-        path: paths.appBuild,
-        filename: "[hash]_[name].js",
-    },
-    optimization: {
-        minimizer: [new UglifyJsPlugin()],
-        runtimeChunk: "single",
-        splitChunks: {
-            chunks: "all",
-            maxInitialRequests: Infinity,
-            minSize: 0,
-            cacheGroups: {
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name(module) {
-                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-                        return `npm.${packageName.replace("@", "")}`;
+module.exports = function (env, argv) {
+    const isEnvProduction = argv.mode === "production";
+
+    return {
+        entry: {
+            main: paths.appIndexJs,
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+        },
+        output: {
+            path: paths.appBuild,
+            filename: "[hash]_[name].js",
+        },
+        optimization: {
+            minimize: isEnvProduction,
+            minimizer: [new UglifyJsPlugin()],
+            runtimeChunk: "single",
+            splitChunks: {
+                chunks: "all",
+                maxInitialRequests: Infinity,
+                minSize: 0,
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name(module) {
+                            const packageName = module.context.match(
+                                /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
+                            )[1];
+                            return `npm.${packageName.replace("@", "")}`;
+                        },
                     },
                 },
             },
         },
-    },
-    devtool: "eval", // fastest mode for development
-    devServer: {
-        contentBase: "./src/",
-        open: false,
-        port: PORT,
-        overlay: {
-            warnings: true,
-            errors: true,
+        devtool: "eval", // fastest mode for development
+        devServer: {
+            contentBase: "./src/",
+            open: false,
+            port: PORT,
+            overlay: {
+                warnings: true,
+                errors: true,
+            },
+            quiet: false,
+            noInfo: true, // hide build info in console
+            watchContentBase: true, // reload page if static files change
+            after() {
+                chalkAnimation.rainbow(`Its Wednesday my dudes! http://${HOST}:${PORT}\n`, 0.3);
+            },
         },
-        quiet: false,
-        noInfo: true, // hide build info in console
-        watchContentBase: true, // reload page if static files change
-        after() {
-            chalkAnimation.rainbow(`Its Wednesday my dudes! http://${HOST}:${PORT}\n`, 0.3);
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: ["babel-loader", "eslint-loader"],
+                },
+                {
+                    test: /\.(png|svg|jpg)$/,
+                    use: ["file-loader"],
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    include: [path.resolve(paths.appSrc, "styles")],
+                    use: ["style-loader", "css-loader", "sass-loader"],
+                },
+            ],
         },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: ["babel-loader", "eslint-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg)$/,
-                use: ["file-loader"],
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                include: [path.resolve(paths.appSrc, "styles")],
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+            }),
+            new CopyPlugin([{ from: paths.appAssets, to: "assets" }]),
+            new ForkTsCheckerWebpackPlugin(),
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
-        new CopyPlugin([{ from: paths.appAssets, to: "assets" }]),
-    ],
+    };
 };
